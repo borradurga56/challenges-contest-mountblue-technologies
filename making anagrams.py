7218073ba9b2a79e21e652d598c5ce1c098112d1
borradurga56 <158791028+borradurga56@users.noreply.github.com>
2024-07-22 13:12:04 +0530

    Create making anagrams.py

---
+++ making anagrams.py
@@ -0,0 +1,23 @@
+import os
+from collections import Counter
+
+# Complete the makeAnagram function below.
+def makeAnagram(a, b):
+    count_a = Counter(a)
+    count_b = Counter(b)
+
+    difference_a = count_a - count_b
+    difference_b = count_b - count_a
+
+    return sum(difference_a.values()) + sum(difference_b.values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = input()
+    b = input()
+
+    result = makeAnagram(a, b)
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
