5b362e6a18a2b338733aaed7118627a7e8e825ac
borradurga56 <158791028+borradurga56@users.noreply.github.com>
2024-07-22 16:42:41 +0530

    Create Sherlock and Array.py

---
+++ Sherlock and Array.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+     leftsum = 0
+     total = sum(arr)
+     for value in arr:
+        if leftsum == total-value-leftsum:
+            return "YES"
+        leftsum+=value
+     return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
