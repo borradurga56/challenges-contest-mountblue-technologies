b4603ca7d7ef49021e232dadb8d802af74c26583
borradurga56 <158791028+borradurga56@users.noreply.github.com>
2024-07-22 13:18:10 +0530

    Create Counting Sort 2.py

---
+++ Counting Sort 2.py
@@ -0,0 +1,24 @@
+import os
+def countingSort(arr):
+    s = [0 for _ in range(100)]
+    ret = []
+
+    for i in arr:
+        s[i] += 1
+
+    for i in range(len(s)):
+        for j in range(s[i]):
+            ret.append(i)
+
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.close()
