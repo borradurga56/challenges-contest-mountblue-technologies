c95e0a6eedb0c92e87e7960f6366aaabdeba076a
borradurga56 <158791028+borradurga56@users.noreply.github.com>
2024-07-22 16:45:23 +0530

    Create Jim and the orders.py

---
+++ Jim and the orders.py
@@ -0,0 +1,11 @@
+# Time complexity: O(n log n)
+def jimOrders(orders):
+    total = {}
+    for i in range(len(orders)):
+        total[i + 1] = sum(orders[i])
+    return [item[0] for item in sorted(total.items(), key=lambda x: x[1])]
+
+orders = []
+for orders_i in range(int(input().strip())):
+    orders.append([int(orders_temp) for orders_temp in input().strip().split(' ')])
+print (" ".join(map(str, jimOrders(orders))))
