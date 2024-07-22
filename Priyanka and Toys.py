473f4636140126bf0f0c2db0a292185a1b7573ba
borradurga56 <158791028+borradurga56@users.noreply.github.com>
2024-07-22 11:27:37 +0530

    Create Priyanka and Toys.py

---
+++ Priyanka and Toys.py
@@ -0,0 +1,16 @@
+# Priyanka and Toys
+# Time complexity: O(n log n)
+
+def toys(w):
+    w.sort()
+    total = 1
+    free_toys = w[0] + 4
+    for i in range(1, len(w)):
+        if w[i] > free_toys:
+            total += 1
+            free_toys = w[i] + 4
+    return total
+
+n = int(input().strip())
+w = list(map(int, input().strip().split(' ')))
+print(toys(w))
